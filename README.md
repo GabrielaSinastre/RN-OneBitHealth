b3b3322d62b02e087ca995ceefbd678475ebff5d
GabrielaSinastre <gabrielamaria.gaby@hotmail.com>
2023-03-13 17:12:44 -0300

    estilização dos componentes

---
+++ README.md
@@ -0,0 +1,14 @@
+Curso de React Native - One Bit Code (Youtube)
+
+Resumo das aulas:
+Aula 01:
+  - Introdução e Configuração do Ambiente
+
+Aula 02:
+  - Componentes e Estruturação do React Native
+
+Aula 03:
+  - Conceitos de Estilização (CSS)
+
+Aula 04:
+  - Introdução às APIs do React Native
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import ResultImc from './ResultImc/index';
+
+import styles from './style';
 
 export default function Form(){
 
@@ -31,28 +33,34 @@ export default function Form(){
   }
 
   return(
-    <View>
-      <View>
-        <Text>Altura:</Text>
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura:</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setHeight} 
           value={height}
           placeholder='Ex. 1.75'
           keyboardType='numeric'
         />
 
-        <Text>Peso:</Text>
+        <Text style={styles.formLabel}>Peso:</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setWeight}
           value={weight}
           placeholder='Ex. 75.356'
           keyboardType='numeric'
         />
         
-        <Button 
-        onPress={()=> validationImc()}
-        title={textButton}
-        />
+        <TouchableOpacity 
+          style={styles.buttonCalculator}
+          onPress={() => {
+            validationImc()
+          }}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
 
       </View>
       <ResultImc resultImc={messageImc} messageResultImc={imc}/>
